4cd8171c154b625684a3f3df846640d80fbce87a
Anna Beatriz Garcia <annagarcia@id.uff.br>
2022-06-29 21:45:47 -0300

    feat:add req 01

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -3,7 +3,6 @@ const productsService = require('../services/productsSevice');
 const getAllProducts = async (_req, res, next) => {
   try {
     const products = await productsService.getAllProducts();
-
     res.status(200).json(products);
   } catch (err) {
     next(err);
@@ -13,15 +12,10 @@ const getAllProducts = async (_req, res, next) => {
 const getByIdProduct = async (req, res, next) => {
   try {
     const { id } = req.params;
-
-    const product = await productsService.getByIdProduct(Number(id));
-    
-    if (product.length === 0) {
-      res.status(404).json({ message: 'Product not found' });
-    }
-
-    res.status(200).json(...product);
-   } catch (err) {
+    const product = await productsService.getAllProducts(id);
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(404).json({ message: 'Product not found' });
     next(err);
    }
 };
--- services/productsSevice.js
+++ services/productsSevice.js
@@ -1,18 +1,12 @@
 const productsModel = require('../models/productsModel');
 
-const getAllProducts = async () => {
-  const products = await productsModel.getAllProducts();
-
-  return products;
-};
-
-const getByIdProduct = async (id) => {
-  const product = await productsModel.getByIdProduct(id);
-
-  return product;
+const getAllProducts = (id = null) => {
+      if (id) {
+        return productsModel.getAllProducts(id);
+    }
+    return productsModel.getByIdProduct();
 };
 
 module.exports = {
   getAllProducts,
-  getByIdProduct,
 };
